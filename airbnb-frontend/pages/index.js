b66a374edebe29619bf62fc0c2a592862b2c5cfe
OmarBelguith <omar.belghith96@gmail.com>
2021-10-23 10:29:11 +0100

    setting up querying with GROQ

--- airbnb-frontend/pages/index.js
+++ airbnb-frontend/pages/index.js
@@ -1,10 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { sanityClient } from '../sanity';
 
 export default function Home() {
   return (
     <>
     </>
   )
+}
+
+export const getServerSideProps = async (ctx) => {
+  const query = '*[ _type == "property"]'
+  const properties = await sanityClient.fetch(query);
+
+  if( !properties.length){
+    return {
+      props: {
+        properties: [],
+      }
+    }
+  }else {
+    return {
+      props: {
+        properties
+      }
+    }
+  }
 }
